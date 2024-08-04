87b1786f199a0a3b8b98857995696a0b06589ffe
shreyas0924 <shreyasp0924@gmail.com>
2024-08-04 20:15:47 +0530

    added other chains, shadcn, .env.example

--- config/index.tsx
+++ config/index.tsx
@@ -1,20 +1,18 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 import { cookieStorage, createStorage } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, sepolia, polygon, base, polygonMumbai } from "wagmi/chains";
 
 export const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!;
 
 const metadata = {
-  name: "BUIDL The new Internet",
+  name: "BUILD The new Internet",
   description: "AppKit",
   url: "https://web3modal.com", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia] as const;
-
-const wagmiOptions = {};
+const chains = [mainnet, sepolia, base, polygon, polygonMumbai] as const;
 
 export const config = defaultWagmiConfig({
   chains,
@@ -28,7 +26,6 @@ export const config = defaultWagmiConfig({
     email: true,
     socials: ["google", "github", "farcaster", "x"],
     showWallets: true,
-    walletFeatures: true
+    walletFeatures: true,
   },
-  ...wagmiOptions, // Optional - Override createConfig parameters
 });
